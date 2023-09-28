cf9628ac9c3a1af99fd78564d331cad315eb33e2
Razi Taj Mazinani <razitmazinani@gmail.com>
2023-09-28 12:47:30 +0330

    Update YOLO_ultralytics.py

--- YOLO_ultralytics.py
+++ YOLO_ultralytics.py
@@ -1,15 +1,14 @@
 from ultralytics import YOLO
 
 # Model
-#model = torch.hub.load('ultralytics/ultralytics', 'yolov8n', pretrained=True)
-model = YOLO("yolov8x.pt")
+model = YOLO("yolov8x.pt")        #yolov5x    yolov5n6    yolov8n    ...
 
 # Images
-imgs = [r"C:\Datasets\faiss - Copy\pics 2\Polar-bears-ice-floe-Norway.webp",
-        r"C:\Datasets\faiss - Copy\pics 2\merlion.jpg",
-        r"C:\Datasets\faiss - Copy\pics 2\dog (3).JPEG",
-        r"C:\Datasets\faiss\pics 2\dog (10).JPEG",
-        r"C:\Datasets\faiss\pics 2\fish (5).JPEG"]  # batch of images
+imgs = [r"C:\Datasets\pics\Polar-bears-ice-floe-Norway.webp",
+        r"C:\Datasets\pics\merlion.jpg",
+        r"C:\Datasets\pics\dog (3).JPEG",
+        r"C:\Datasets\pics\dog (10).JPEG",
+        r"C:\Datasets\pics\fish (5).JPEG"]  # batch of images
 
 # Inference
 #results = model.predict(imgs)
@@ -27,9 +26,3 @@ for result in results:
         print("Coordinates:", cords)
         print("Probability:", conf)
     print("---")
-
-
-# def show_box(box, ax):
-#     x0, y0 = box[0], box[1]
-#     w, h = box[2] - box[0], box[3] - box[1]
-#     ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
